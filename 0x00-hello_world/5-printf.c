dab5826868867728ca202f7bf41cee6bf5fd9f6a
Marian <bomamarian99@gmail.com>
2023-02-20 09:06:34 -0800

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
